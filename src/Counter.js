2bb8ec2485e0d4c88faecccb20ee29dc22f71707
Adam Klin <adam.klin@assecobs.pl>
2020-07-13 15:16:53 +0200

    hooks

--- src/Counter.js
+++ src/Counter.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Counter.css'
 import Display from './Display'
 import ButtonsPanel from './ButtonsPanel'
 import Step from './Step'
-class Counter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counterValue: this.props.initValue,
-      step: this.props.initStepValue,
-    }
-    this.changeValue = this.changeValue.bind(this);
+
+// handleStep = (value) => {
+//   setStepValue(Number(value));
+//   // this.setState({ step: Number(value) })
+// }
+
+const Counter = (props) => {
+  const [counterValue, setCounterValue] = useState(props.initValue);
+  const [stepValue, setStepValue] = useState(props.initStepValue);
+
+
+  const handleStep = (value) => {
+    setStepValue(Number(value));
+    // this.setState({ step: Number(value) })
   }
 
-  handleStep = (value) => {
-    this.setState({ step: Number(value) })
-  }
-  changeValue(action) {
+  const changeValue = (action) => {
+
     this.setState((prevState, prevProps) => {
       let currentCounterValue = prevState.counterValue;
 
@@ -35,20 +39,21 @@ class Counter extends React.Component {
         { counterValue: currentCounterValue }
       )
     })
-  }
-
-  render() {
-    // let randomNumber = Math.floor(Math.random() * (108) + 1);
-    return (
-      <div className="counter">
-        Licznik :
-        <Display value={this.state.counterValue} />
-        <ButtonsPanel buttonMethod={this.changeValue} step={this.state.step} />
-        <Step step={this.state.step} stepMethod={this.handleStep} />
-      </div>
-    )
 
   }
+
+  // let randomNumber = Math.floor(Math.random() * (108) + 1);
+  return (
+    <div className="counter">
+      Licznik :
+      <Display value={counterValue} />
+      <ButtonsPanel buttonMethod={changeValue} step={stepValue} />
+      <Step step={stepValue} stepMethod={handleStep} />
+    </div>
+  )
+
+
 }
 
 export default Counter
+
