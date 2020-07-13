71750b85d788bddf426035b2c77d0bded7ddf290
Adam Klin <adam.klin@assecobs.pl>
2020-07-13 09:58:58 +0200

    css

--- src/ButtonsPanel.js
+++ src/ButtonsPanel.js
@@ -4,9 +4,9 @@ const ButtonPanels = (props) => {
   return (
     <div className="buttons-panel">
       <button onClick={() => props.buttonMethod('add')}>Add 1</button>
+      <button onClick={() => props.buttonMethod('step')}>Zwiększ o : {props.step}</button>
       <button onClick={() => props.buttonMethod('reinit')}>Reinit</button>
       <button onClick={() => props.buttonMethod('reset')}>Reset</button>
-      <button onClick={() => props.buttonMethod('step')}>Zwiększ o : {props.step}</button>
     </div>
   )
 }
