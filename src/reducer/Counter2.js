492cbbf9a5a5ab5a68dd2652454060ffee4e7ab0
Adam Klin <adam.klin@assecobs.pl>
2020-07-15 13:49:35 +0200

    works

--- src/reducer/Counter2.js
+++ src/reducer/Counter2.js
@@ -1,10 +1,11 @@
 import React, { Component, useReducer } from 'react';
 import { reducer, initialState } from './Reducer'
+import { useState } from 'react';
 
 const Counter2 = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
-  console.log(state);
+  // console.log(state);
   return (
     <>
       <h1>Reducer Counter</h1>
@@ -12,12 +13,12 @@ const Counter2 = () => {
         Licznik: <span className='value'> {state.result}</span>
         <div>
           <button onClick={() => dispatch({ type: 'add' })}>Add 1</button>
-          <button onClick={() => dispatch({ type: 'addStep', step: 5 })}>Zwiększ o: </button>
+          <button onClick={() => dispatch({ type: 'addStep' })}>Zwiększ o: {state.step}</button>
           <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
           <button onClick={() => dispatch({ type: 'reInit' })}>Reinit</button>
-
         </div>
-
+        {/* <Step step={state.step} /> */}
+        <input type="number" name="number" min="1" value={state.step} onChange={(e) => dispatch({ type: 'setStep', step: e.target.value })} />
       </div>
     </>
   );
--- src/reducer/Reducer.js
+++ src/reducer/Reducer.js
@@ -1,18 +1,18 @@
 import React, { useReducer } from 'react'
 
 const initialState = {
-  init: 1,
-  result: 1
+  result: 10,
+  step: 5
 }
 
 const reducer = (state, action) => {
-  // console.log(state);
   switch (action.type) {
     case 'add': return { ...state, result: state.result + 1 }
-    case 'addStep': return { ...state, result: state.result + action.step }
-    case 'reset': return { ...state };
-    // case 'reInit': return initialState;
-    // default: throw new Error('Wystąpił błąd...');
+    case 'addStep': return { ...state, result: state.result + Number(state.step) }
+    case 'reset': return { ...state, result: 0, step: 1 };
+    case 'setStep': return { ...state, step: action.step };
+    case 'reInit': return initialState;
+
     default:
       throw new Error('Unexpected action');
   }
